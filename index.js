d6af41f97d4d51b2f404591ba673d97b96ed3011
Katherine Peterson <octokatherine@github.com>
2021-10-21 22:14:32 -0700

    remove unneeded code

--- index.js
+++ index.js
@@ -50,25 +50,23 @@ function formatEvent(event) {
   switch (type) {
     case 'IssuesEvent':
       if (payload.action === 'opened') {
-        return `:issue-new: Opened "${payload.issue.title}" ${payload.issue.html_url}`
+        return `:issue-new: Opened ${payload.issue.title}: ${payload.issue.html_url}`
       }
     case 'PullRequestReviewEvent':
       if (payload.action === 'created') {
-        return `:mag: Reviewed "${payload.pull_request.title}" ${payload.pull_request.html_url}`
+        return `:mag: Reviewed ${payload.pull_request.title}: ${payload.pull_request.html_url}`
       }
     case 'PullRequestEvent':
       if (payload.action === 'opened') {
-        return `:pr-open: Opened "${payload.pull_request.title}" ${payload.pull_request.html_url}`
+        return `:pr-open: Opened ${payload.pull_request.title}: ${payload.pull_request.html_url}`
       } else if (payload.action === 'review_requested') {
-        return `:ready-for-review: Put up for review "${payload.pull_request.title}" ${payload.pull_request.html_url}`
+        return `:ready-for-review: Put up for review ${payload.pull_request.title}: ${payload.pull_request.html_url}`
       } else if (payload.action === 'closed' && payload.pull_request && payload.pull_request.merged) {
-        return `:pr-merged: Merged "${payload.pull_request.title}" ${payload.pull_request.html_url}`
+        return `:pr-merged: Merged ${payload.pull_request.title}: ${payload.pull_request.html_url}`
       } else if (payload.action === 'closed' && payload.pull_request && !payload.pull_request.merged) {
-        return `:pr-closed: Closed "${payload.pull_request.title}" ${payload.pull_request.html_url}`
+        return `:pr-closed: Closed ${payload.pull_request.title}: ${payload.pull_request.html_url}`
       }
   }
-
-  return type + payload.action
 }
 
 main()
